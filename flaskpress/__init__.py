c9651488993be85b7edfabcb105cb1d7b96edb0e
allan swanepoel <Allanice001@gmail.com>
2017-09-10 06:31:43 +0200

    flaskpress.core should be mostly complete

--- flaskpress/__init__.py
+++ flaskpress/__init__.py
@@ -3,7 +3,54 @@
 import warnings
 from flask.exthook import ExtDeprecationWarning
 from flaskpress.core.admin import create_admin
-
+from flaskpress.core.app import FlaskpressApp  # noqa
+from flaskpress.core.middleware import HTTPMethodOverrideMiddleware  # noqa
+from flaskpress.ext import configure_extensions, configure_extension  # noqa
 warnings.simplefilter("ignore", category=ExtDeprecationWarning)
 
 admin = create_admin()
+
+
+def create_app_base(config=None, test=False, admin_instance=None,
+                    ext_list=None, **settings):
+    app = FlaskpressApp('flaskpress')
+    app.config.load_flaskpress_config(config=config, test=test, **settings)
+    if test or app.config.get('TESTING'):
+        app.testing = True
+    if ext_list:
+        for ext in ext_list:
+            configure_extension(ext, app=app)
+    return app
+
+
+def create_app(config=None, test=False, admin_instance=None, **settings):
+    app = create_app_base(
+        config=config, test=test, admin_instance=admin_instance, **settings
+    )
+
+    configure_extensions(app, admin_instance or admin)
+    if app.config.get("HTTP_PROXY_METHOD_OVERRIDE"):
+        app.wsgi_app = HTTPMethodOverrideMiddleware(app.wsgi_app)
+    return app
+
+
+def create_api(config=None, **settings):
+    return None
+
+
+def create_celery_app(app=None):
+    from celery import Celery
+    app = app or create_app()
+    celery = Celery(__name__, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    taskbase = celery.Task
+
+    class ContextTask(taskbase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return taskbase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
---
+++ flaskpress/core/config.py
@@ -0,0 +1,74 @@
+import os
+import logging
+import flaskpress.core.models as m
+from flask.config import Config
+from flaskpress.utils import parse_conf_data
+from cached_property import cached_property_ttl, cached_property
+
+logger = logging.getLogger()
+
+
+class FlaskpressConfig(Config):
+    @cached_property
+    def store(self):
+        return dict(self)
+
+    @cached_property_ttl(300)
+    def all_setings_from_db(self):
+        try:
+            return {
+                item.name: item.value
+                for item in m.config.Config.objects.get(
+                group='settings'
+            ).values
+            }
+        except Exception as e:
+            logger.warning('Error reading all settings from db: %s' % e)
+            return {}
+
+    def get_from_db(self, key, default=None):
+        return self.all_setings_from_db.get(key, default)
+
+    def __getitem__(self, key):
+        return self.get_from_db(key) or dict.__getitem__(self, key)
+
+    def get(self, key, default=None):
+        return self.get_from_db(key) or self.store.get(key) or default
+
+    def from_object(self, obj, silent=False):
+        try:
+            super(FlaskpressConfig, self).from_object(obj)
+        except ImportError as e:
+            if silent:
+                return False
+            e.message = 'Unable to load configuration obj (%s)' % e.message
+            raise
+
+    def from_envvar_namespace(self, namespace='FLASKPRESS', silent=False):
+        try:
+            data = {
+                key.partition('_')[-1]: parse_conf_data(data)
+                for key, data
+                in os.environ.items()
+                if key.startswith(namespace)
+            }
+            self.update(data)
+        except Exception as e:
+            if silent:
+                return False
+            e.message = 'Unable to load config env namespace (%s)' % e.message
+            raise
+
+    def load_flaskpress_config(self, config=None, mode=None, test=None, **sets):
+        self.from_object(config or 'flaskpress.settings')
+        mode = mode or 'test' if test else os.environ.get(
+            'FLASKPRESS_MODE', 'local')
+        self.from_object('flaskpress.%s_settings' % mode, silent=True)
+        path = "FLASKPRESS_SETTINGS" if not test else "FLASKPRESSTEST_SETTINGS"
+        self.from_envvar(path, silent=True)
+        self.from_envvar_namespace(namespace='FLASKPRESS', silent=True)
+        self.update(sets)
+
+
+
+
