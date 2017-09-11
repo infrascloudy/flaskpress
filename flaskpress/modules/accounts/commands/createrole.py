dcc343cb0d7e774eda63dd104d6bd2fe98c2ce41
allan swanepoel <Allanice001@gmail.com>
2017-09-11 09:50:07 +0200

    flaskpress.modules.accounts mostly complete

---
+++ flaskpress/modules/accounts/commands/createrole.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import click
+from ..models import Role
+
+
+@click.command()
+@click.option('--name', help='Role name', prompt=True)
+@click.option('--description', help='Role description', prompt=True)
+def cli(name, description):
+    if all([name, description]):
+        role = Role.createrole(
+            name=name,
+            description=description
+        )
+    else:
+        role = "Cant create the role"
+
+    click.echo(role)
